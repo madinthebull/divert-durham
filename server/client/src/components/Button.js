27fac6531885843c4a06a74e98f7941177d381a9
mcstreilein <madelinestreilein@Madelines-MacBook-Air.local>
2019-05-06 20:58:44 -0400

    create reusable submit button and basic form on Profile component

---
+++ server/client/src/components/Button.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class Button extends Component {
+  render() {
+    const { formClick, title } = this.props
+    return (
+      <div className="btn" onClick={formClick}>
+        {title}
+      </div>
+    )
+  }
+}
+
+export default Button
--- server/client/src/components/Profile.js
+++ server/client/src/components/Profile.js
@@ -1,9 +1,99 @@
 import React, { Component } from 'react'
+import SubmitButton from './Button'
 
-export class Profile extends Component {
+class Profile extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: 'madinthebull',
+      email: 'mcstreilein@gmail.com',
+      firstName: 'Madeline',
+      lastName: 'Streilein',
+      address: '915B Exum Street Durham NC 27701',
+      profileType: 'Giver'
+    }
+  }
   render() {
-    return <div>This is the profile component</div>
+    return (
+      <React.Fragment>
+        <div className="mask show" />
+        <div className="form-container show">
+          <div className="form-card show">
+            <div className="form-header-container">
+              <div className="form-header">
+                <div className="form-name">edit profile</div>
+                <div className="fas fa-times" />
+              </div>
+            </div>
+            <form className="form-field-container">
+              {' '}
+              <p className="">Username</p>
+              <input
+                type="text"
+                name="username"
+                className="form-field"
+                placeholder="Enter user name"
+                value={this.state.username}
+                style={inputStyle}
+              />
+              <p className="">email address</p>
+              <input
+                type="email"
+                name="email"
+                className="form-field"
+                placeholder="Enter email address"
+                value={this.state.email}
+                style={inputStyle}
+              />
+              <p className="">first name</p>
+              <input
+                type="text"
+                name="firstName"
+                className="form-field"
+                placeholder="Enter first name"
+                value={this.state.firstName}
+                style={inputStyle}
+              />
+              <p className="">last name</p>
+              <input
+                type="text"
+                name="lastName"
+                className="form-field"
+                placeholder="Enter last name"
+                value={this.state.lastName}
+                style={inputStyle}
+              />
+              <p className="">home address</p>
+              <input
+                type="text"
+                name="address"
+                className="form-field"
+                placeholder="Enter address"
+                value={this.state.address}
+                style={inputStyle}
+              />
+              <p className="">profile type</p>
+              <input
+                type="text"
+                name="profileTyle"
+                className="form-field"
+                placeholder="Enter profile Type"
+                value={this.state.profileType}
+                readOnly
+              />
+              <div className="form-footer-container">
+                <SubmitButton />
+              </div>
+            </form>
+          </div>
+        </div>
+      </React.Fragment>
+    )
   }
 }
 
+const inputStyle = {
+  backgroundColor: '#ddd',
+  borderRadius: '5px'
+}
 export default Profile
